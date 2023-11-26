ea627ee7e53416251b197a8d21c1fb4dab625dc0
toongri <jhkim03284@gmail.com>
2023-11-26 19:45:35 +0900

    프로젝트 개설

---
+++ src/test/java/petalk/mvp/MvpApplicationTests.java
@@ -0,0 +1,13 @@
+package petalk.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
