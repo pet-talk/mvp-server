487ddbbf00bb9e7fa17a3f349eef1bbee3ff873e
toongri <jhkim03284@gmail.com>
2023-12-11 23:21:23 +0900

    유효성 검사 오류 정보 구현
    ValidationError class 추가
    
    유효성 검사 오류 시에 필드와 메세지를 저장합니다.

---
+++ src/main/java/petalk/mvp/core/ValidationError.java
@@ -0,0 +1,37 @@
+package petalk.mvp.core;
+
+/**
+ * 유효성 검사 오류 정보를 나타내는 클래스입니다.
+ */
+public class ValidationError {
+    private String field;
+    private String message;
+
+    public ValidationError(String field, String message) {
+        this.message = message;
+        this.field = field;
+    }
+
+    private ValidationError() {
+    }
+
+    public boolean hasName() {
+        return message != null;
+    }
+
+    public static ValidationError of(String field) {
+        return new ValidationError(field, null);
+    }
+
+    public static ValidationError of(String field, String message) {
+        return new ValidationError(field, message);
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
