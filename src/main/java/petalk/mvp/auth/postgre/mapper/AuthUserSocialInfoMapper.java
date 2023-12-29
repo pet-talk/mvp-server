7fd514e48c9eb5ca0a2e0485d922c9ec3d637808
toongri <jhkim03284@gmail.com>
2023-12-30 01:30:19 +0900

    AuthUserSocialInfoMapper 클래스 리팩터링
    AuthUserSocialInfoMapper의 메서드가 개별 User 및 SocialAuthUser 객체가 아닌 단일 UserSocialInfo 객체를 받도록 리팩터링되었습니다. 이렇게 하면 메서드 서명이 간소화되고 객체 지향 설계 원칙에 부합하게 됩니다.

--- src/main/java/petalk/mvp/auth/postgre/mapper/AuthUserSocialInfoMapper.java
+++ src/main/java/petalk/mvp/auth/postgre/mapper/AuthUserSocialInfoMapper.java
@@ -1,14 +1,13 @@
 package petalk.mvp.auth.postgre.mapper;
 
-import petalk.mvp.auth.domain.SocialAuthUser;
-import petalk.mvp.auth.domain.User;
+import petalk.mvp.auth.domain.UserSocialInfo;
 import petalk.mvp.auth.postgre.model.AuthUserSocialInfoJpa;
 import petalk.mvp.core.Mapper;
 
 @Mapper
 public class AuthUserSocialInfoMapper {
 
-    public AuthUserSocialInfoJpa from(User user, SocialAuthUser socialAuthUser) {
-        return new AuthUserSocialInfoJpa(user.getId().getValue(), socialAuthUser.getEmail(), socialAuthUser.getSocialType(), socialAuthUser.getSocialId().getValue(), socialAuthUser.getName());
+    public AuthUserSocialInfoJpa from(UserSocialInfo userSocialInfo) {
+        return new AuthUserSocialInfoJpa(userSocialInfo.getUserId().getValue(), userSocialInfo.getEmail(), userSocialInfo.getSocialType(), userSocialInfo.getSocialId().getValue(), userSocialInfo.getSocialName());
     }
 }
