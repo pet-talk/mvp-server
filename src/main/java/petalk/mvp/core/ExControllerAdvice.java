19e9b946975dfa47b19b56d4489dcbb150c588d4
toongri <jhkim03284@gmail.com>
2024-01-03 18:18:53 +0900

    예외 처리기에 로깅 추가
    이 커밋은 ExControllerAdvice 클래스의 예외 핸들러에 로깅을 도입합니다. 로거는 예외가 발생할 때마다 오류 메시지를 기록하여 애플리케이션의 디버깅 및 오류 추적 프로세스를 개선합니다.

--- src/main/java/petalk/mvp/core/ExControllerAdvice.java
+++ src/main/java/petalk/mvp/core/ExControllerAdvice.java
@@ -1,5 +1,7 @@
 package petalk.mvp.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(annotations = RestController.class)
 public class ExControllerAdvice {
 
+  private final Logger logger = LoggerFactory.getLogger(ExControllerAdvice.class);
+
 
   @ExceptionHandler
   public ResponseEntity<ApiResponse<ParameterValids>> exHandler(ValidationErrorException e) {
@@ -16,18 +20,26 @@ public class ExControllerAdvice {
     ParameterValids valids = ParameterValids.from(e.getErrors());
     ApiResponse<ParameterValids> response = ApiResponse.validationError(valids);
 
+    logger.error(e.getMessage(), e);
+
     return ResponseEntity.badRequest().body(response);
   }
 
   @ExceptionHandler
   public ResponseEntity<ApiResponse<String>> exHandler(RuntimeException e) {
     ApiResponse<String> response = ApiResponse.fail(e.getMessage());
+
+    logger.error(e.getMessage(), e);
+
     return ResponseEntity.badRequest().body(response);
   }
 
   @ExceptionHandler
   public ResponseEntity<ApiResponse<String>> exHandler(Exception e) {
     ApiResponse<String> response = ApiResponse.fail(e.getMessage());
+
+    logger.error(e.getMessage(), e);
+
     return ResponseEntity.internalServerError().body(response);
   }
 
