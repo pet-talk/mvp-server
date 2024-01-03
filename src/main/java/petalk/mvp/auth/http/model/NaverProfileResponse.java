95835bb62352b4ef52ba8d8bd799907ae1550e7b
toongri <jhkim03284@gmail.com>
2024-01-03 19:23:02 +0900

    기능 개선을 위한 NaverProfileResponse 리팩터링
    이번 커밋에서는 사용성과 기능을 개선하기 위해 NaverProfileResponse 클래스가 리팩터링되었습니다. 이제 HTTP 상태 코드, 메시지, 사용자 정보를 위한 추가 필드와 새로운 응답 처리 방식이 포함됩니다. 이 변경은 네이버 API의 프로필 응답 데이터를 보다 투명하고 유연하게 처리하는 것을 목표로 합니다.

--- src/main/java/petalk/mvp/auth/http/model/NaverProfileResponse.java
+++ src/main/java/petalk/mvp/auth/http/model/NaverProfileResponse.java
@@ -1,6 +1,9 @@
 package petalk.mvp.auth.http.model;
 
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
@@ -10,33 +13,58 @@ import java.util.Optional;
  *
  * 응답에 대한 상태를 확인할 수 있습니다.
  */
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class NaverProfileResponse {
 
-    private final ResponseEntity<String> response;
+    private HttpStatusCode statusCode;
+    @SerializedName("message")
+    private String message;
+    @SerializedName("response")
+    private UserInfo user;
 
-    private NaverProfileResponse(ResponseEntity<String> response) {
-        this.response = response;
+
+    public static NaverProfileResponse from(ResponseEntity<String> response, Gson gson) {
+        NaverProfileResponse profileResponse = gson.fromJson(response.getBody(), NaverProfileResponse.class);
+        profileResponse.addResponse(response);
+        return profileResponse;
     }
 
-    public static NaverProfileResponse from(ResponseEntity<String> response) {
-        return new NaverProfileResponse(response);
+    private void addResponse(ResponseEntity<String> response) {
+        this.statusCode = response.getStatusCode();
     }
 
     public boolean isOk() {
-        return response.getStatusCode().is2xxSuccessful();
+        return statusCode.is2xxSuccessful();
     }
     public boolean isFailed() {
-        return !response.getStatusCode().is2xxSuccessful();
+        return !isOk();
     }
 
-    public Optional<SocialProfile> mapProfile(Gson gson) {
+    public Optional<SocialProfile> mapProfile() {
         if(isOk()) {
-            return Optional.of(toSocialProfile(gson));
+            return Optional.of(toSocialProfile());
         }
         return Optional.empty();
     }
 
-    private NaverProfile toSocialProfile(Gson gson) {
-        return gson.fromJson(response.getBody(), NaverProfile.class);
+    private NaverProfile toSocialProfile() {
+
+        return new NaverProfile(statusCode.toString(), message, user.id, user.email, user.nickname, user.name, user.gender, user.age, user.birthday, user.profileImage, user.birthYear, user.mobile);
+    }
+
+    @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+    static class UserInfo {
+        private String id;
+        private String email;
+        private String nickname;
+        private String name;
+        private String gender;
+        private String age;
+        private String birthday;
+        @SerializedName("profile_image")
+        private String profileImage;
+        @SerializedName("birthyear")
+        private String birthYear;
+        private String mobile;
     }
 }
