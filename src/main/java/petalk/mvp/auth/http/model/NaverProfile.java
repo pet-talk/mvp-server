4b0d74ae88c58f181dd46629dfdc51e4d2a60cdd
toongri <jhkim03284@gmail.com>
2024-02-01 17:18:29 +0900

    네이버 프로필 객체 생성 메서드 리팩터링
    네이버 프로필 객체를 단순화 했습니다.
    멤버 변수였던 resultCode나 message등이 현재 필요없다고 판단하여 삭제했고, 좀 더 프로필과 관련이 있는 값들을 정적 중첩 클래스를 제거하고 직접적으로 멤버변수에 추가했습니다.
    추가적으로 생성자로 생성하던 객체를 of 정적 생성함수를 추가하여 통일성을 높였습니다.

--- src/main/java/petalk/mvp/auth/http/model/NaverProfile.java
+++ src/main/java/petalk/mvp/auth/http/model/NaverProfile.java
@@ -1,7 +1,5 @@
 package petalk.mvp.auth.http.model;
 
-import com.google.gson.annotations.SerializedName;
-import lombok.Builder;
 import petalk.mvp.auth.domain.NaverSocialAuthUser;
 import petalk.mvp.auth.domain.SocialAuthId;
 import petalk.mvp.auth.domain.SocialAuthUser;
@@ -10,71 +8,49 @@ import petalk.mvp.auth.domain.SocialAuthUser;
  * Naver 프로필을 나타냅니다.
  */
 public class NaverProfile implements SocialProfile {
-    private String resultCode;
-    private String message;
-    private Profile response;
+    private String id;
+    private String email;
+    private String nickname;
+    private String name;
+    private String gender;
+    private String age;
+    private String birthday;
+    private String profileImage;
+    private String birthYear;
+    private String mobile;
+
+    private NaverProfile(String id, String email, String nickname, String name, String gender, String age, String birthday, String profileImage, String birthYear, String mobile) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.birthday = birthday;
+        this.profileImage = profileImage;
+        this.birthYear = birthYear;
+        this.mobile = mobile;
+    }
+
+    public static NaverProfile of(String id, String email, String nickname, String name, String gender, String age, String birthday, String profileImage, String birthYear, String mobile) {
+        NaverProfile naverProfile = new NaverProfile(id, email, nickname, name, gender, age, birthday, profileImage, birthYear, mobile);
+        naverProfile.validate();
+        return naverProfile;
+    }
 
     @Override
     public SocialAuthUser toSocialAuthUser() {
-        SocialAuthId id = SocialAuthId.from(response.id);
-        return NaverSocialAuthUser.from(id, response.email, response.nickname, response.name);
+        SocialAuthId socialAuthId = SocialAuthId.from(id);
+        return NaverSocialAuthUser.from(socialAuthId, email, nickname, name);
     }
 
-    public NaverProfile(String resultCode, String message, String id, String email, String nickname, String name, String gender, String age, String birthday, String profileImage, String birthyear, String mobile) {
-        this.resultCode = resultCode;
-        this.message = message;
-        Profile profile = Profile.builder()
-                .id(id)
-                .email(email)
-                .nickname(nickname)
-                .name(name)
-                .gender(gender)
-                .age(age)
-                .birthday(birthday)
-                .profileImage(profileImage)
-                .birthYear(birthyear)
-                .mobile(mobile)
-                .build();
-        profile.validate();
-        this.response = profile;
-    }
-
-    private static class Profile {
-        private String id;
-        private String email;
-        private String nickname;
-        private String name;
-        private String gender;
-        private String age;
-        private String birthday;
-        @SerializedName("profile_image")
-        private String profileImage;
-        @SerializedName("birthyear")
-        private String birthYear;
-        private String mobile;
-
-        @Builder
-        private Profile(String id, String email, String nickname, String name, String gender, String age, String birthday, String profileImage, String birthYear, String mobile) {
-            this.id = id;
-            this.email = email;
-            this.nickname = nickname;
-            this.name = name;
-            this.gender = gender;
-            this.age = age;
-            this.birthday = birthday;
-            this.profileImage = profileImage;
-            this.birthYear = birthYear;
-            this.mobile = mobile;
+    public void validate() {
+        if (id == null || email == null || nickname == null || name == null) {
+            throw new IllegalArgumentException("Naver 프로필이 올바르지 않습니다.");
         }
 
-        public void validate() {
-            if (id == null || email == null || nickname == null || name == null) {
-                throw new IllegalArgumentException("Naver 프로필이 올바르지 않습니다.");
-            }
-
-            if (id.isBlank() || email.isBlank() || nickname.isBlank() || name.isBlank()) {
-                throw new IllegalArgumentException("Naver 프로필이 올바르지 않습니다.");
-            }
+        if (id.isBlank() || email.isBlank() || nickname.isBlank() || name.isBlank()) {
+            throw new IllegalArgumentException("Naver 프로필이 올바르지 않습니다.");
         }
     }
 }
