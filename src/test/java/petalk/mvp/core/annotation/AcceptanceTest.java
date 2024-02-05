7ba3d56513f62dbf23927c84b5f301df349e7b6f
toongri <jhkim03284@gmail.com>
2024-02-06 02:26:58 +0900

    인수테스트와 유닛테스트 분리

--- src/test/java/petalk/mvp/core/annotation/AcceptanceTest.java
+++ src/test/java/petalk/mvp/core/annotation/AcceptanceTest.java
@@ -2,6 +2,7 @@ package petalk.mvp.core.annotation;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Tag;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
@@ -9,6 +10,7 @@ import java.lang.annotation.*;
 /**
  * 인수 테스트 클래스임을 명시하는 어노테이션
  */
+@Tag("acceptanceTest")
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
