f0394942924329e3a4d387ce461cdca7763a352d
toongri <jhkim03284@gmail.com>
2024-02-07 14:20:55 +0900

    헬스체크 컨트롤러 추가

---
+++ src/main/java/petalk/mvp/present/healthcheck/HealthCheckController.java
@@ -0,0 +1,15 @@
+package petalk.mvp.present.healthcheck;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Hidden
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "OK";
+    }
+}
