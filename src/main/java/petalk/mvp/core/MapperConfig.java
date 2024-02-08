ec9f267c4b858823efcdb3294ad876be982b6f4e
toongri <jhkim03284@gmail.com>
2024-02-09 06:16:38 +0900

    base64 빈에 등록

--- src/main/java/petalk/mvp/core/MapperConfig.java
+++ src/main/java/petalk/mvp/core/MapperConfig.java
@@ -2,6 +2,7 @@ package petalk.mvp.core;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,5 +26,10 @@ public class MapperConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public Base64 base64() {
+        return new Base64();
     }
 }
