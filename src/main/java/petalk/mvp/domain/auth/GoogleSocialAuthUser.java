91415005082e75c880a90424e3306b250b008ae2
toongri <jhkim03284@gmail.com>
2024-02-17 12:12:11 +0900

    구글소셜인증사용자에 닉네임 필드 추가하기
    구글소셜인증 사용자 클래스에 닉네임 필드가 추가되었습니다. 이 새로운 필드는 이제 클래스의 생성자 및 팩토리 메서드에 포함되며 사용자 등록 시에도 활용됩니다. 이렇게 하면 사용자 등록 시 닉네임을 고려할 수 있습니다.

--- src/main/java/petalk/mvp/domain/auth/GoogleSocialAuthUser.java
+++ src/main/java/petalk/mvp/domain/auth/GoogleSocialAuthUser.java
@@ -11,24 +11,26 @@ public class GoogleSocialAuthUser implements SocialAuthUser{
     private SocialAuthId socialAuthId;
     private String email;
     private String name;
+    private String nickname;
     private SocialType socialType;
 
     //== 생성 메소드 ==//
 
-    private GoogleSocialAuthUser(SocialAuthId socialAuthId, String email, String name) {
+    private GoogleSocialAuthUser(SocialAuthId socialAuthId, String email, String name, String nickname) {
         this.socialAuthId = socialAuthId;
         this.email = email;
         this.name = name;
         this.socialType = SocialType.GOOGLE;
+        this.nickname = nickname;
     }
 
-    public static GoogleSocialAuthUser from(SocialAuthId socialAuthId, String email, String name) {
-        return new GoogleSocialAuthUser(socialAuthId, email, name);
+    public static GoogleSocialAuthUser from(SocialAuthId socialAuthId, String email, String name, String nickname) {
+        return new GoogleSocialAuthUser(socialAuthId, email, name, nickname);
     }
     //== 비즈니스 로직 ==//
     @Override
     public AuthUser registerUser(LocalDateTime registrationDate) {
-        return AuthUser.register(this.email, this.socialType, this.socialAuthId, this.name, this.name, registrationDate);
+        return AuthUser.register(this.email, this.socialType, this.socialAuthId, this.name, this.nickname, registrationDate);
     }
     //== 수정 메소드 ==//
     //== 조회 메소드 ==//
