4448b245d0f11a77986cdeb54beb0f245451c5f5
toongri <jhkim03284@gmail.com>
2024-02-18 16:16:46 +0900

    SessionUserModel 이름 Provider로 이름 변경

--- src/main/java/petalk/mvp/storage/auth/adapter/RegisterSessionAdapter.java
+++ src/main/java/petalk/mvp/storage/auth/adapter/RegisterSessionAdapter.java
@@ -5,9 +5,9 @@ import lombok.RequiredArgsConstructor;
 import petalk.mvp.application.auth.command.out.RegisterSessionPort;
 import petalk.mvp.domain.auth.SessionUserInfo;
 import petalk.mvp.storage.auth.mapper.SessionUserModelMapper;
-import petalk.mvp.storage.postgre.SessionRepository;
+import petalk.mvp.support.auth.SessionRepository;
 import petalk.mvp.core.PersistenceAdapter;
-import petalk.mvp.support.auth.SessionUserModel;
+import petalk.mvp.support.auth.Provider;
 
 @PersistenceAdapter
 @RequiredArgsConstructor
@@ -18,7 +18,7 @@ public class RegisterSessionAdapter implements RegisterSessionPort {
 
     @Override
     public void registerSession(SessionUserInfo user, HttpServletRequest httpServletRequest) {
-        SessionUserModel model = mapper.from(user);
+        Provider model = mapper.from(user);
         sessionRepository.save(model, httpServletRequest);
     }
 }
--- src/main/java/petalk/mvp/support/auth/Provider.java
+++ src/main/java/petalk/mvp/support/auth/Provider.java
@@ -3,19 +3,19 @@ package petalk.mvp.support.auth;
 import java.util.UUID;
 
 /**
- * 세션에 저장되어있는 유저 정보를 담고있는 클래스입니다.
+ * 요청한 유저 정보를 담고있는 클래스입니다.
  */
-public class SessionUserModel {
+public class Provider {
     private UUID userId;
     private AuthRoleAuthority userAuthority;
 
-    private SessionUserModel(UUID userId, AuthRoleAuthority userAuthority) {
+    private Provider(UUID userId, AuthRoleAuthority userAuthority) {
         this.userId = userId;
         this.userAuthority = userAuthority;
     }
 
-    public static SessionUserModel of(UUID userId, AuthRoleAuthority userAuthority) {
-        return new SessionUserModel(userId, userAuthority);
+    public static Provider of(UUID userId, AuthRoleAuthority userAuthority) {
+        return new Provider(userId, userAuthority);
     }
 
     public UUID getUserId() {
