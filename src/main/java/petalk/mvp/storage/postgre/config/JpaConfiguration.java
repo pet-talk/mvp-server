d7300181fcd42474427bbc16b287d9af12123cc9
toongri <jhkim03284@gmail.com>
2024-02-18 16:21:16 +0900

    jpa configuration 분리

---
+++ src/main/java/petalk/mvp/storage/postgre/config/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package petalk.mvp.storage.postgre.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfiguration {
+}
