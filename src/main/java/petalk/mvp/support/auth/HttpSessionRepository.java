3eb5b888bd3a1f90b75cf8916dcf4c9b5b42e8ff
toongri <jhkim03284@gmail.com>
2024-02-20 20:14:15 +0900

    HashMapSessionRepository에서 HttpSessionRepository 로 이름 변경

--- src/main/java/petalk/mvp/support/auth/HttpSessionRepository.java
+++ src/main/java/petalk/mvp/support/auth/HttpSessionRepository.java
@@ -3,13 +3,17 @@ package petalk.mvp.support.auth;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.util.Optional;
+
 @Component
+@Slf4j
 @RequiredArgsConstructor
-public class HashMapSessionRepository implements SessionRepository {
+public class HttpSessionRepository implements SessionRepository {
     private final String HTTP_SESSION_KEY = "session";
     @Override
     public void save(Provider model) {
@@ -17,5 +21,18 @@ public class HashMapSessionRepository implements SessionRepository {
         HttpServletRequest request = attr.getRequest();
         HttpSession session = request.getSession();
         session.setAttribute(HTTP_SESSION_KEY, model);
+    }
+
+    @Override
+    public Optional<Provider> getProvider(String sessionId) {
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = attr.getRequest();
+        HttpSession session = request.getSession();
+
+        Provider attribute = (Provider) session.getAttribute(HTTP_SESSION_KEY);
+        if (attribute == null) {
+            return Optional.empty();
+        }
+        return Optional.of(attribute);
     }
 }
