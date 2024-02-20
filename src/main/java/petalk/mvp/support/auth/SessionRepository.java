fb048bab47d7c8e048f5e3d21cb2f9fdcef21493
toongri <jhkim03284@gmail.com>
2024-02-20 20:16:59 +0900

    세션 저장소에 getProvider 메서드 추가
    'Optional<Provider> getProvider(String sessionId)' 메서드가 SessionRepository 인터페이스에 추가되었습니다. 이 메서드 추가는 세션저장소에서 세션아이디로 공급자를 직접 가져올 수 있는 옵션을 제공합니다.

--- src/main/java/petalk/mvp/support/auth/SessionRepository.java
+++ src/main/java/petalk/mvp/support/auth/SessionRepository.java
@@ -1,6 +1,10 @@
 package petalk.mvp.support.auth;
 
+import java.util.Optional;
+
 public interface SessionRepository {
 
     void save(Provider sessionModel);
+
+    Optional<Provider> getProvider(String sessionId);
 }
